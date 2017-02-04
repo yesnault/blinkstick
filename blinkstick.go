e3944754a8271fbbc1f5164af43eebba5eb0137e
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2017-02-04 23:16:08 +0100

    [auto] bump version to v0.0.2

--- blinkstick.go
+++ blinkstick.go
@@ -10,7 +10,7 @@ import (
 // Version of Blinkstick
 // One Line for this, used by release.sh script
 // Keep "const Version on one line"
-const Version = "0.0.1"
+const Version = "0.0.2"
 
 // VendorID blinkstick
 const VendorID = 0x20a0
